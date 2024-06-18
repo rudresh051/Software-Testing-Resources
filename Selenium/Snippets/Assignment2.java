794684dfbd9487c64bc8e4220f998a8a19447f4a
Rudreshwar Jha <56826499+rudresh051@users.noreply.github.com>
2024-06-18 23:54:23 +0530

    script updated

--- Selenium/Snippets/Assignment2.java
+++ Selenium/Snippets/Assignment2.java
@@ -22,27 +22,53 @@ public class Assignment2 {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://v1.training-support.net/selenium/tab-opener");
 		driver.findElement(By.id("launcher")).click();
-		Thread.sleep(3000);
+		Thread.sleep(2000);
+		
 		// Step 1 - Switch the control from parent window to child window
 		String child_tab1 = "https://v1.training-support.net/selenium/newtab";
+		String child_tab2 = "https://v1.training-support.net/selenium/newtab2";
 		String parent_tab = driver.getWindowHandle();
 		
 		Set<String> all_tab_ids = driver.getWindowHandles();
-		System.out.println(all_tab_ids);
-		
+//		System.out.println(all_tab_ids);
 //		System.out.println(parent_tab);
 		
+		
+		// Switching to 2nd tab
 		for (String var1 : all_tab_ids) {
 			System.out.println(var1);
 			driver.switchTo().window(var1);
 			if (driver.getCurrentUrl().contains(child_tab1)) {
+				System.out.println(driver.getCurrentUrl() + " " + var1);
+//				System.out.println(driver.getCurrentUrl().contains(child_tab1));
 				break;
 			}
 			
 		}
 		driver.findElement(By.id("actionButton")).click();
-		Thread.sleep(2000);
+		
+		// Close the first child tab
 		driver.close();
+		
+		// Switching to third tab
+		Set<String> all_tab_ids1 = driver.getWindowHandles();
+		for (String var2 : all_tab_ids1) {
+			driver.switchTo().window(var2);
+			if(driver.getCurrentUrl().contains(child_tab2)) {
+				System.out.println(driver.getCurrentUrl().contains(child_tab2));
+				break;
+			}
+		}
+		
+		Thread.sleep(2000);
+		driver.navigate().refresh(); // Thread.sleep(2000) is added to so that refresh is visible to the user.
+		Thread.sleep(2000);
+		
+		// Close the 2nd child tab
+		driver.close();
+		
+		// Close the parent tab
+		driver.quit();
 
 	}
 
